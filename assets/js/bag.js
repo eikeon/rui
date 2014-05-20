b80f8b4ea12b213c5430f46909e5d80f6c94aff5
Daniel Krech <eikeon@eikeon.com>
2014-05-20 11:00:00 -0400

    Cleaned up name

--- assets/js/bag.js
+++ assets/js/bag.js
@@ -8,20 +8,24 @@ var React = require('react'),
 
 Bag = React.createClass({
     render: function () {
-        var fs = {backgroundImage: "url("+this.props.file.image_large+")"};
-        return <div className="foo">
-                 <div className="image" style={fs}>
-                 </div>
-                 <div className="previous">
-                   <h1><Previous onItemChanged={this.props.onItemChanged} item={this.props.file} items={this.props.files} /></h1>
-                 </div>
-                 <div className="current">
-                   <ItemNumber file={this.props.file} files={this.props.files} />
-                 </div>
-                 <div className="next">
-                   <h1><Next onItemChanged={this.props.onItemChanged} item={this.props.file} items={this.props.files} /></h1>
-                 </div>
-               </div>;
+        if (this.props.file) {
+            var fs = {backgroundImage: "url("+this.props.file.image_large+")"};
+            return <div className="bagitem">
+                     <div className="image" style={fs}>
+                     </div>
+                     <div className="previous">
+                       <h1><Previous onItemChanged={this.props.onItemChanged} item={this.props.file} items={this.props.files} /></h1>
+                     </div>
+                     <div className="current">
+                       <ItemNumber file={this.props.file} files={this.props.files} />
+                     </div>
+                     <div className="next">
+                       <h1><Next onItemChanged={this.props.onItemChanged} item={this.props.file} items={this.props.files} /></h1>
+                     </div>
+                    </div>;
+        } else {
+            return <div><p>...</p></div>;
+        }
     }
 });
 

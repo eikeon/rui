9bb590391af55dce9d867a53132de3519b9711ea
Daniel Krech <eikeon@eikeon.com>
2014-12-02 15:41:08 -0500

    Moved npm and bower install later into AfterInstall script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,5 +3,3 @@ yum groupinstall -y "Development tools" "Development Libraries"
 curl -sL https://rpm.nodesource.com/setup | bash -
 yum install -y nodejs
 npm install -g bower gulp
-cd /var/rui; bower --allow-root install
-cd /var/rui; npm install
